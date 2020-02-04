535edaff14b374a4e7c8ef8b87a50c97d114e0bd
sponsbob10 <rezaadnan62@gmail.com>
2020-02-04 15:45:26 +0700

    hari kedua

--- novice/02-02/kasus/soal3.dart
+++ novice/02-02/kasus/soal3.dart
@@ -1,1 +1,14 @@
-void main
+
+void main (){
+print(full());
+}
+int full(){
+  var maxvalue = [1,2,3,7,8];
+  var max = maxvalue [1];
+  for (var i = maxvalue[1] + 1; i < maxvalue.length; i++){
+    if (maxvalue[i] > maxvalue[max]){
+      max = i;
+    }
+  }
+  return max;
+}
