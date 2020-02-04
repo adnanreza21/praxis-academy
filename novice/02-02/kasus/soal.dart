3d99387151247197de8dcf40f7622da486be4593
sponsbob10 <rezaadnan62@gmail.com>
2020-02-04 11:55:39 +0700

    hari kedua

--- novice/02-02/kasus/soal.dart
+++ novice/02-02/kasus/soal.dart
@@ -1,4 +1,16 @@
 void main() {
-  if (3>2)
+  var a = [1,2,3,4,5];
 
+  for (var i = 0; i < a.length; i++){
+    var key = a[i];
+    var j = i;
+    while (j > 0 && a[j - 1] > key){
+      a[j]= a[ j- 1];
+      j= j - 1;
+    }
+    a[j]=key; 
+  }
+
+  print(a);
+  
 }
